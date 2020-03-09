ca1ea31869d4bbe5c60f94004dc3d44972dd9c13
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-08 20:35:19 -0700

    overloading contructors passed

---
+++ src/main/java/Classes/Picture.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class Picture {
+    private boolean framed;
+    private int height;
+    private int width;
+
+    public Picture(int height, int width) {
+        this(false,height, width);
+    }
+
+    Picture(boolean framed, int height, int width) {
+        this.framed = framed;
+        this.height = height;
+        this.width = width;
+    }
+
+
+}
---
+++ src/test/java/Classes/PictureTest.java
@@ -0,0 +1,16 @@
+package Classes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PictureTest {
+    @Test
+    public void TestPicture() {
+        //Setup
+        int height = 1;
+        int width = 2;
+        Picture picture = new Picture(height, width);
+    }
+
+}
