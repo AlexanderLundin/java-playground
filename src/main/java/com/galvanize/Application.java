1e5d0f5f6990c95d276e420a98397bf8fe128be8
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-13 22:25:00 -0700

    lambdas

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,7 +1,16 @@
 package com.galvanize;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        List<String> fileNames = Arrays.asList("My Report.csv", "The Best Deck.pdf", "Thanks for all the fishes.doc");
+        fileNames.forEach(fileName -> {
+            String newFileName = fileName.replace(" ", "-");
+            System.out.println(newFileName);
+        });
+        System.out.println(fileNames);
     }
 }
