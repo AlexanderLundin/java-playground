c8ef53510b5106b1ec79622b0cc4ce1613f4fba6
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-16 15:34:27 -0700

    try catch

--- src/main/java/Exceptions/Exceptions.java
+++ src/main/java/Exceptions/Exceptions.java
@@ -2,3 +2,9 @@ package Exceptions;
 
 public class Exceptions {
 }
+
+class RiskyProcess{
+    int run(int input) throws Exception {
+            throw new Exception();
+    }
+}
--- src/test/java/Exceptions/ExceptionsTest.java
+++ src/test/java/Exceptions/ExceptionsTest.java
@@ -1,7 +1,28 @@
 package Exceptions;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ExceptionsTest {
-
+    @Test
+    public void TestRiskyProcess() throws Exception {
+        //Setup
+        int input = 1;
+        RiskyProcess riskyProcess = new RiskyProcess();
+        //Exercise
+        try{
+            riskyProcess.run(input);
+        }catch(NullPointerException e){
+            System.out.println("encountered null pointer");
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("index out of bounds");
+        }catch(Exception e){
+            System.out.println("Exception occurred");
+        }finally {
+            System.out.println("Done"); // always print this, no matter what
+        }
+        //Assert
+        //Teardown
+    }
 }
